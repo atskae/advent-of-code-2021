ab7f0d00fec6c1a73f762d73c59ec8161c1e9551
Atsuko Shimizu <atskae@gmail.com>
2021-12-02 05:51:04 -0500

    Add day-2 part 2

--- solutions/day-2/solution.py
+++ solutions/day-2/solution.py
@@ -9,10 +9,6 @@ def get_horizontal_pos_and_depth() -> tuple[int, int]:
     final_depth = 0
 
     for index, (direction, amount) in enumerate(steps):
-        if index < 10:
-            print(f"{index}: final_horizontal_pos={final_horizontal_pos}, final_depth={final_depth}")
-            print(f"{index}: direction={direction}, amount={amount}")
-
         if direction == "forward":
             final_horizontal_pos += amount
         elif direction == "down":
@@ -22,11 +18,44 @@ def get_horizontal_pos_and_depth() -> tuple[int, int]:
         else:
             print(f"Unknown direction: {direction}. Skipping...")
 
+        if index < 10:
+            print(f"{index}: final_horizontal_pos={final_horizontal_pos}, final_depth={final_depth}")
+            print(f"{index}: direction={direction}, amount={amount}")
+
+    return final_horizontal_pos, final_depth
+
+
+def get_horizontal_pos_and_depth_with_aim() -> tuple[int, int]:
+    global steps
+    final_horizontal_pos = 0
+    final_depth = 0
+    aim = 0
+
+    for index, (direction, amount) in enumerate(steps):
+        if direction == "forward":
+            final_horizontal_pos += amount
+            final_depth += (aim * amount)
+        elif direction == "down":
+            aim += amount
+        elif direction == "up":
+            aim -= amount
+        else:
+            print(f"Unknown direction: {direction}. Skipping...")
+
+        if index < 10:
+            print(f"{index}: final_horizontal_pos={final_horizontal_pos}, final_depth={final_depth}")
+            print(f"{index}: direction={direction}, amount={amount}, aim={aim}")
+
     return final_horizontal_pos, final_depth
 
 
 def main():
     horizontal_pos, depth = get_horizontal_pos_and_depth()
+    print(f'horizontal_pos={horizontal_pos}, depth={depth}')
+    print(f'horizontal_pos * depth = {horizontal_pos * depth}')
+
+    horizontal_pos, depth = get_horizontal_pos_and_depth_with_aim()
+    print('With aim:')
     print(f'horizontal_pos={horizontal_pos}, depth={depth}')
     print(f'horizontal_pos * depth = {horizontal_pos * depth}')
 
