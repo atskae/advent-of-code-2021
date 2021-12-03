1b66ac659d4b77902046bdb4ad78222afb70ac43
Atsuko Shimizu <atskae@gmail.com>
2021-12-03 07:00:16 -0500

    Add day-3 part 2 solutions

--- solutions/day-3/solution.py
+++ solutions/day-3/solution.py
@@ -22,11 +22,12 @@ def get_rates_for_power_consumption(binary_nums: list[str]) -> tuple[int, int]:
     return gamma_rate, epsilon_rate
 
 
-def get_rates_for_life_support(binary_nums: list[str]) -> tuple[int, int]:
+def get_rate_for_life_support(binary_nums: list[str], get_oxygen_generator=True) -> int:
     num_columns = len(binary_nums[0])
-    selected_binary_nums = deepcopy(binary_nums)  # copy since will modify
-    for column in range(0, num_columns):
 
+    selected_binary_nums = deepcopy(binary_nums)  # copy since will modify
+    rating = 0
+    for column in range(0, num_columns):
         matches_with_zero = []
         matches_with_one = []
         for binary_num in selected_binary_nums:
@@ -36,18 +37,43 @@ def get_rates_for_life_support(binary_nums: list[str]) -> tuple[int, int]:
             else:
                 matches_with_one.append(binary_num)
 
+        if get_oxygen_generator:
+            # Compute the oxygen generator rating
+            if len(matches_with_zero) > len(matches_with_one):
+                selected_binary_nums = matches_with_zero
+            else:
+                selected_binary_nums = matches_with_one
+        else:
+            # Compute the CO2 scrubber rating
+            if len(matches_with_one) < len(matches_with_zero):
+                selected_binary_nums = matches_with_one
+            else:
+                selected_binary_nums = matches_with_zero
+
+        if len(selected_binary_nums) == 1:
+            rating = selected_binary_nums[0]
+            break
+
+    return int(rating, 2)
+
 
 def main():
-    input_file = open("example_input.txt", "r")
+    input_file = open("input.txt", "r")
     binary_nums = [(line.strip()) for line in input_file]
 
     gamma_rate, epsilon_rate = get_rates_for_power_consumption(binary_nums)
     formatter = f"0{len(binary_nums[0])}b"
     print(f"gamma_rate={gamma_rate}, {format(gamma_rate, formatter)}")
     print(f"epsilon_rate={epsilon_rate}, {format(epsilon_rate, formatter)}")
-
     power_consumption = gamma_rate * epsilon_rate
     print(f"power_consumption: {power_consumption}")
+
+    oxygen_generator_rating = get_rate_for_life_support(binary_nums, get_oxygen_generator=True)
+    co2_scrubber_rating = get_rate_for_life_support(binary_nums, get_oxygen_generator=False)
+    print(f"oxygen_generator_rating: {oxygen_generator_rating}")
+    print(f"co2_scrubber_rating: {co2_scrubber_rating}")
+    life_support_rating = oxygen_generator_rating * co2_scrubber_rating
+    print(f"life_support_rating: {life_support_rating}")
 
 
 main()
