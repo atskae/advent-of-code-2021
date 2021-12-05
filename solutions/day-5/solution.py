9bafd89f5cb7ebf7fdd8c97b301a0abb71d25476
Atsuko Shimizu <atskae@gmail.com>
2021-12-05 08:37:23 -0500

    Create separate FieldMap objects for each part

--- solutions/day-5/solution.py
+++ solutions/day-5/solution.py
@@ -3,31 +3,15 @@ import re
 
 class FieldMap:
 
-    def __init__(self, input_file_name):
-        self.map = {}
-        self.lines = []
-        self.num_overlaps = 0
-
-        input_file = open(input_file_name, "r")
-        for file_line in input_file:
-            file_line = file_line.strip()
-            points_str = [point.strip() for point in file_line.split("->")]
-            re_pattern = r"(\d+),(\d+)"
-            line = []
-            for point_str in points_str:
-                matches = re.match(re_pattern, point_str)
-                point = (int(matches.group(1)), int(matches.group(2)))
-                line.append(point)
-            self.lines.append(line)
-
-        # Fill in the map
+    def _fill_map(self, skip_diagonals=True):
         for line in self.lines:
             (x1, y1) = line[0]
             (x2, y2) = line[1]
-            #print(f"({x1},{y1}) -> ({x2},{y2})")
-            if not (x1 == x2 or y1 == y2):
-                #print("Skipping")
-                continue
+            # print(f"({x1},{y1}) -> ({x2},{y2})")
+            if skip_diagonals:
+                if not (x1 == x2 or y1 == y2):
+                    # print("Skipping")
+                    continue
 
             step = 1
             if x1 == x2:
@@ -43,8 +27,8 @@ class FieldMap:
                 if x1 > x2:
                     step = -1
 
-            #print(f"p1={p1}, p2+step={p2+step}, step={step}")
-            for z in range(p1, p2+step, step):
+            # print(f"p1={p1}, p2+step={p2+step}, step={step}")
+            for z in range(p1, p2 + step, step):
                 if x1 == x2:  # vertical line
                     p = (x1, z)
                 else:
@@ -57,16 +41,39 @@ class FieldMap:
                 if self.map[p] == 2:
                     self.num_overlaps += 1
 
-                #print(f"{p}: {self.map[p]}")
+                # print(f"{p}: {self.map[p]}")
+
+    def __init__(self, lines, skip_diagonals=True):
+        self.map = {}
+        self.lines = lines
+        self.num_overlaps = 0
+        self._fill_map(skip_diagonals=skip_diagonals)
+
+
+def parse_input_file(input_file_name: str) -> list[list[tuple[int, int]]]:
+    input_file = open(input_file_name, "r")
+    lines = []
+    for file_line in input_file:
+        file_line = file_line.strip()
+        points_str = [point.strip() for point in file_line.split("->")]
+        re_pattern = r"(\d+),(\d+)"
+        line = []
+        for point_str in points_str:
+            matches = re.match(re_pattern, point_str)
+            point = (int(matches.group(1)), int(matches.group(2)))
+            line.append(point)
+        lines.append(line)
+    return lines
 
 
 def main():
     input_file_name = "input.txt"
     print(f"Reading in: {input_file_name}")
-    field_map = FieldMap(input_file_name)
+    lines = parse_input_file(input_file_name)
+    field_map_no_diagonals = FieldMap(lines, skip_diagonals=True)
     #for point in sorted(field_map.map.keys()):
     #    print(f"{point}: {field_map.map[point]}")
-    print("num_overlaps", field_map.num_overlaps)
+    print("num_overlaps", field_map_no_diagonals.num_overlaps)
 
 
 main()
