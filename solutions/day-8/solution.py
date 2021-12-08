f2a0a04133adc8469c5504953a374306dfca2cea
Atsuko Shimizu <atskae@gmail.com>
2021-12-08 06:24:37 -0500

    Add day-8 part 1 solutions

--- solutions/day-8/solution.py
+++ solutions/day-8/solution.py
@@ -1,4 +1,39 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DigitEntry:
+    signals: list[str]
+    outputs: list[str]
+
+
+def parse_input_file(input_file_name) -> list[DigitEntry]:
+    print(f"Reading input: {input_file_name}")
+    digit_entries = []
+    with open(input_file_name, "r") as f:
+        for line in f:
+            line = line.split("|")
+            signal_patterns = line[0].split()
+            digit_outputs = line[1].strip().split()
+            digit_entries.append(DigitEntry(signals=signal_patterns, outputs=digit_outputs))
+
+    return digit_entries
+
+
 def main():
+    input_file_name = "input.txt"
+    digit_entries = parse_input_file(input_file_name)
+
+    total_unique = 0
+    for digit in digit_entries:
+        print(f"signals: {digit.signals}")
+        print(f"outputs: {digit.outputs}")
+        for output in digit.outputs:
+            num_segments = len(output)
+            if num_segments == 2 or num_segments == 3 or num_segments == 4 or num_segments == 7:
+                total_unique += 1
+    print(f"Total unique: {total_unique}")
+
     digits = {
         0: ['a', 'b', 'c', 'e', 'f', 'g'],
         1: ['c', 'f'],
