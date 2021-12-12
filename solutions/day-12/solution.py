674dd82d63e44ce68901ba6383ddceddf89f51db
Atsuko Shimizu <atskae@gmail.com>
2021-12-12 07:08:35 -0500

    Add day-12 part 2 solutions

--- solutions/day-12/solution.py
+++ solutions/day-12/solution.py
@@ -23,7 +23,7 @@ def parse_input_file(input_file_name) -> dict[str, list[str]]:
     return cave_graph
 
 
-def _r_find_paths(all_paths, cave_graph, visited, cave, path) -> list[str]:
+def _r_find_paths(all_paths, cave_graph, visited, cave, path):
     if cave in visited and cave.islower():
         return
     if cave == "start":
@@ -39,11 +39,35 @@ def _r_find_paths(all_paths, cave_graph, visited, cave, path) -> list[str]:
         _r_find_paths(all_paths, cave_graph, dict(visited), adj_cave, current_path[:])
 
 
+def _r_find_new_paths(all_paths, cave_graph, visited, special_small_cave, cave, path):
+    if cave in visited and cave.islower():
+        if cave != special_small_cave:
+            return
+        else:
+            if visited[cave] == 2:
+                return
+    if cave == "start":
+        return
+    if cave == "end":
+        final_path = tuple(path[:] + [cave])
+        all_paths.add(final_path)
+        return
+
+    if cave not in visited:
+        visited[cave] = 0
+    visited[cave] += 1
+
+    current_path = path[:] + [cave]
+    for adj_cave in cave_graph[cave]:
+        _r_find_new_paths(all_paths, cave_graph, dict(visited), special_small_cave, adj_cave, current_path[:])
+
+
 def main(argv):
     cave_graph = parse_input_file(argv[1])
     for cave in cave_graph.keys():
         print(f"{cave} -> {cave_graph[cave]}")
 
+    # Part 1
     all_paths = set()
     for cave in cave_graph["start"]:
         _r_find_paths(all_paths, cave_graph, {}, cave, ["start"])
@@ -52,6 +76,24 @@ def main(argv):
         print(path)
 
     print(f"Found {len(all_paths)} paths")
+
+    # Part 2
+    small_caves = []
+    for cave in cave_graph.keys():
+        if cave.islower() and (cave != "start" and cave != "end"):
+            small_caves.append(cave)
+    print(f"All small caves: {small_caves}")
+
+    all_new_paths = set()
+    # The special_small_cave can be visited at most twice
+    for special_small_cave in small_caves:
+        for cave in cave_graph["start"]:
+            _r_find_new_paths(all_new_paths, cave_graph, {}, special_small_cave, cave, ["start"])
+
+    for path in all_new_paths:
+        print(path)
+
+    print(f"Found {len(all_new_paths)} new paths")
 
 
 if __name__ == "__main__":
