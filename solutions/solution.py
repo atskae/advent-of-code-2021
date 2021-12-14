af1fc4389306c5a22bae7ea5b6dee5748e574d18
Atsuko Shimizu <atskae@gmail.com>
2021-12-14 05:31:07 -0500

    Add template solution script

---
+++ solutions/solution.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def parse_input_file(input_file_name):
+    print(f"Reading input: {input_file_name}")
+
+
+def main(argv):
+    parse_input_file(argv[1])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
