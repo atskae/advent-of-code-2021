f32e526cd0ef3400d2dad4072c6936480129c9bd
Atsuko Shimizu <atskae@gmail.com>
2021-12-14 05:33:05 -0500

    Update template

--- solutions/solution.py
+++ solutions/solution.py
@@ -3,6 +3,9 @@ import sys
 
 def parse_input_file(input_file_name):
     print(f"Reading input: {input_file_name}")
+    with open(input_file_name, "r") as f:
+        for line in f:
+            pass
 
 
 def main(argv):
